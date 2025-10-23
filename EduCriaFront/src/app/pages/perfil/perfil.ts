b4e8ad51b8d2595db9118029cf7476fab4d1ae91
JULIA SOARES DE QUEIROGA <24153592@edu119.local>
2025-10-23 16:02:51 -0300

    componente perfil feito funcionando obs: falta login continuo e arrumar botao de voltar e scrollar a tela

--- EduCriaFront/src/app/pages/perfil/perfil.ts
+++ EduCriaFront/src/app/pages/perfil/perfil.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './perfil.css'
 })
 export class Perfil {
+  constructor(private router: Router) {}  
 
+  goBack() {
+    this.router.navigate(['/home']);
+  }
 }
+
+
